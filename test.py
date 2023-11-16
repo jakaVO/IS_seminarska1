dbb8155202cb5f73512f137d1ba680b4bebb71cb
markmihelic <mm2318@student.uni-lj.si>
2023-11-16 23:09:24 +0100

    correction of fitness function and reading the csv cd /Users/admin/Documents/FAKS/IS/IS_seminarska1 ; /usr/bin/env /opt/homebrew/bin/python3.9 /Users/admin/.vscode/extensions/ms-python.python-2023.20.0/pythonFiles/lib/python/debugpy/adapter/../../debugpy/launcher 54361 -- /Users/admin/Documents/FAKS/IS/IS_seminarska1/test.py

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+# Treba za naredit:
+# Pri generate_random_tree ne sme pridt do digit to digit operacij (enkrat sm dubu 9**5 kot del expressiona)
+
 from functions import *
 import numpy as np
 import random
@@ -5,39 +8,74 @@ import pandas as pd
 
 solution = ""
 
-df = pd.read_csv("data.txt")
-selected_row = df.iloc[29]
-x_values = np.array(selected_row["Xs"])
-y_values = np.array(selected_row["Ys"])
+row_index = 41
+df = pd.read_csv("data.csv")
+x_values = df["Xs"].iloc[row_index]
+y_values = df["Ys"].iloc[row_index]
+correct_solution = df["Equation"].iloc[row_index]
+x_values = np.array(eval(x_values))
+y_values = np.array(eval(y_values))
+
 
 population_size = 100
-max_generations = 100
-mutation_rate = 1
-number_of_parents = 10
-max_base_population_tree_height = 5
+max_generations = 10
+mutation_rate = 2
+number_of_parents = 1
+max_base_population_tree_height = 1
 
 def fitness(expression):
     try:
-        predicted_y = [evaluate(expression, xi) for xi in x_values]
+        predicted_y = []
+        print("fitness")
+        print_expression(expression)
+        list_x = x_values.tolist()
+        for xi in list_x:
+            yi = evaluate(expression, int(xi))
+            predicted_y.append(yi)
         mse = np.mean((np.array(predicted_y) - y_values) ** 2)
-        return (1.0 / (mse + 1e-3), expression)  # Avoid division by zero
+        print("mse: " + str(mse))
+        return (1.0 / (mse + 1e-6), expression)  # Avoid division by zero
     except Exception as e:
         return (0.0, expression)  # Return a low fitness for invalid expressions
 
+mytree = TreeNode("+")
+mytree.left = TreeNode("/")
+mytree.left.left = TreeNode("x")
+mytree.left.right = TreeNode("3")
+mytree.right = TreeNode("9")
+
+mytree2 = TreeNode("/")
+mytree2.left = TreeNode("x")
+mytree2.right = TreeNode("3")
+
+population = []
+population.append(mytree)
+population.append(mytree2)
 # Create an initial population
-population = [generate_random_tree(max_base_population_tree_height) for _ in range(population_size)]
+population += [generate_random_tree(max_base_population_tree_height) for _ in range(population_size)]
+for i in range(len(population)):
+    print_expression(population[i])
 
 # Main genetic algorithm loop
 for generation in range(max_generations):
+    print("zacetek nove")
     # Evaluate fitness for each individual
     fitness_scores = [fitness(individual) for individual in population]
+    print([x[0] for x in fitness_scores])
 
     # Select parents based on fitness scores (roulette wheel selection)
     selected_parents = sorted(fitness_scores, key=lambda x: x[0], reverse=True)[:number_of_parents]
 
-    if fitness(selected_parents[0][1]) == 1000.0:
-        population = selected_parents
+    print("Pa še selected:")
+    for i in range(len(selected_parents)):
+        print_expression(selected_parents[i][1])
+
+    if selected_parents[0][0] == 1000000.0:
+        population = [x[1] for x in selected_parents]
+        print("najdena")
         break
+
+    print("asd")
 
     # Create a new population through crossover and mutation
     new_population = [t[1] for t in selected_parents]
@@ -64,7 +102,14 @@ for generation in range(max_generations):
 
     population = new_population
 
+    print("Nova populacija:")
+    for i in range(len(population)):
+        print_expression(population[i])
+
 best_individual = population[0]
+print(population)
 
 print("Best Equation:")
 print_expression(best_individual)
+print("Correct equation:")
+print(correct_solution)
